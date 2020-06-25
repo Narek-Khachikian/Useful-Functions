c4ed3a425ce51ddb82b822f7c992cc92f5bbdb99
Narek Khachikian <narekbarev@hotmail.com>
2020-06-25 16:57:06 +0400

    Add project files.

---
+++ ClassLibraryForTest/Morse.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace NarekLibrary.ForTest
+{
+    /// <summary>
+    /// This class will convert text(case insensitive) to Morse code with EncodeToMorse, and will convert Morse code to text
+    /// with DecodeFromMorse. Note that Morse code contains ' - ' (dash), ' . ' (dot), '   ' (space) and ' / '(slash).
+    /// In encoded text, space means end of character and slash means end of word.
+    /// </summary>
+    public class Morse
+    {
+        /// <summary>
+        /// Encodes a text to morse code. It supports limited characters and it is case insensitive. In case of unsuported characters
+        /// result will be notification.
+        /// </summary>
+        /// <param name="str">English text, with limited special caracter support</param>
+        /// <returns>Morse code</returns>
+        public static string EncodeToMorse(string str)
+        {
+            
+            Dictionary<char, string> morse = new Dictionary<char, string>();
+            #region Morse Dictionary
+            morse.Add('a', ".-");
+            morse.Add('b', "-...");
+            morse.Add('c', "-.-.");
+            morse.Add('d', "-..");
+            morse.Add('e', ".");
+            morse.Add('f', "..-.");
+            morse.Add('g', "--.");
+            morse.Add('h', "....");
+            morse.Add('i', "..");
+            morse.Add('j', ".---");
+            morse.Add('k', "-.-");
+            morse.Add('l', ".-..");
+            morse.Add('m', "--");
+            morse.Add('n', "-.");
+            morse.Add('o', "---");
+            morse.Add('p', ".--.");
+            morse.Add('q', "--.-");
+            morse.Add('r', ".-.");
+            morse.Add('s', "...");
+            morse.Add('t', "-");
+            morse.Add('u', "..-");
+            morse.Add('v', "...-");
+            morse.Add('w', ".--");
+            morse.Add('x', "-..-");
+            morse.Add('y', "-.--");
+            morse.Add('z', "--..");
+            morse.Add('0', "-----");
+            morse.Add('1', ".----");
+            morse.Add('2', "..---");
+            morse.Add('3', "...--");
+            morse.Add('4', "....-");
+            morse.Add('5', ".....");
+            morse.Add('6', "-....");
+            morse.Add('7', "--...");
+            morse.Add('8', "---..");
+            morse.Add('9', "----.");
+            morse.Add('!', "-.-.--");
+            morse.Add(',', "--..--");
+            morse.Add('\'', ".----.");
+            morse.Add('.', ".-.-.-");
+            morse.Add('?', "..--..");
+            morse.Add(':', "---...");
+            morse.Add(' ', "/");
+            #endregion
+
+
+            StringBuilder result = new StringBuilder();
+            foreach (char c in str.ToLower())
+            {
+                string st;
+                result.Append(" ");
+                if (morse.TryGetValue(c, out st)) 
+                { 
+                    result.Append(st); 
+                }
+                else
+                {
+                    StringBuilder keys = new StringBuilder();
+                    char[] chars = morse.Keys.ToArray<char>();
+                    foreach(char ct in chars)
+                    {
+                        keys.Append(ct);
+                        keys.Append(' ');
+                    }
+                    return "Notification - Your text contains characters that are not supported by this tool. These are the supported characters\n" + keys.ToString();
+                }
+            }
+            return result.ToString().Substring(1);
+        }
+
+
+
+
+        /// <summary>
+        /// Decodes a Morse code to English text. It supports limited characters and it returns a text in lower case. In case of unsuported characters
+        /// result will be notification.
+        /// </summary>
+        /// <param name="input">Morse code, with limited character support</param>
+        /// <returns>English text</returns>
+        public static string DecodeFromMorse(string input)
+        {
+            Dictionary<string, char> morse = new Dictionary<string, char>();
+            #region Morse Dictionary
+            morse.Add(".-", 'a');
+            morse.Add("-...", 'b');
+            morse.Add("-.-.", 'c');
+            morse.Add("-..", 'd');
+            morse.Add(".", 'e');
+            morse.Add("..-.", 'f');
+            morse.Add("--.", 'g');
+            morse.Add("....", 'h');
+            morse.Add("..", 'i');
+            morse.Add(".---", 'j');
+            morse.Add("-.-", 'k');
+            morse.Add(".-..", 'l');
+            morse.Add("--", 'm');
+            morse.Add("-.", 'n');
+            morse.Add("---", 'o');
+            morse.Add(".--.", 'p');
+            morse.Add("--.-", 'q');
+            morse.Add(".-.", 'r');
+            morse.Add("...", 's');
+            morse.Add("-", 't');
+            morse.Add("..-", 'u');
+            morse.Add("...-", 'v');
+            morse.Add(".--", 'w');
+            morse.Add("-..-", 'x');
+            morse.Add("-.--", 'y');
+            morse.Add("--..", 'z');
+            morse.Add("-----", '0');
+            morse.Add(".----", '1');
+            morse.Add("..---", '2');
+            morse.Add("...--", '3');
+            morse.Add("....-", '4');
+            morse.Add(".....", '5');
+            morse.Add("-....", '6');
+            morse.Add("--...", '7');
+            morse.Add("---..", '8');
+            morse.Add("----.", '9');
+            morse.Add("-.-.--", '!');
+            morse.Add("--..--", ',');
+            morse.Add(".----.", '\'');
+            morse.Add(".-.-.-", '.');
+            morse.Add("..--..", '?');
+            morse.Add("---...", ':');
+            morse.Add("/", ' ');
+            #endregion
+
+            if (input.Length == 0) { return "Notification - Input is empty"; }
+            if(Regex.IsMatch(input, @"^[^.\- /]+$")) { return "Notification - Input contains unsoported charecters used in Morse code.\n" + "Morse code should contain ' - ' (dash), ' . ' (dot), '   ' (space) and ' / '(slash)."; }
+            string[] inputArr = input.Split(' ');
+            StringBuilder result = new StringBuilder();
+            foreach (string str in inputArr)
+            {
+                char c;
+                if (morse.TryGetValue(str,out c))
+                {
+                    result.Append(c);
+                }
+                else
+                {
+                    return "Notification - Mose code may contains unsuported characters or invalid codes. Please check your code.";
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ ClassLibraryForTest/Prime.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace NarekLibrary.ForTest
+{
+    /// <summary>
+    /// A class to deal with prime numbers. It supports integer numbers.
+    /// </summary>
+    class Prime
+    {
+        /// <summary>
+        /// Checks if the given integer number is prime or not. Note that 0 and 1 are not prime numbers. Returns true for primes.
+        /// </summary>
+        /// <param name="value"> Integer number</param>
+        /// <returns>True/False</returns>
+        public static bool IsPrime(int value)
+        {
+            if (value <= 1) { return false; }
+            if (value == 2 || value == 3) { return true; }
+            if (value % 2 == 0) { return false; }
+            int squar = (int)Math.Ceiling(Math.Sqrt((double)value));
+            for (int i = 3; i <= squar; i = i + 2)
+            {
+                if (value % i == 0) { return false; }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Returns the next prime in range of Integer numbers. In case of large numbers, close to int.MaxValue, result might be 2.
+        /// </summary>
+        /// <param name="num">Integer number</param>
+        /// <returns>Next prime in range of Integer numbers</returns>
+        public int nextPrime(int num)
+        {
+            if (num < 2) { return 2; }
+            if (num < 3) { return 3; }
+            if (num < 5) { return 5; }
+            if (num < 7) { return 7; }
+
+            
+            while (!IsPrime(++num)) ;
+            return num;
+        }
+
+        /// <summary>
+        /// Returns an array of Integer numbers, containing all prime factors of the given number. It returns 0 for numbers smaller than 2.
+        /// Example'    25 -> 5 x 5
+        ///             12 -> 2 x 2 x 3
+        ///             14 -> 2 x 7
+        ///             11 -> 11
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns>An array of prime factors of the given number</returns>
+        public int[] primeFactors(int num)
+        {
+            List<int> result = new List<int>();
+            if (num < 2)
+            {
+                result.Add(0);
+                return result.ToArray();
+            }
+            int prime = 2;
+            while (num / prime >= 1)
+            {
+                if (num % prime == 0)
+                {
+                    result.Add(prime);
+                    num = num / prime;
+                }
+                else
+                {
+                    prime = nextPrime(prime);
+                }
+            }
+            return result.ToArray();
+
+        }
+    }
+}
